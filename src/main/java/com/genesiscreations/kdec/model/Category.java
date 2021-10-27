260c26de824fb7a1dddb18f19f098b1ef5992d1b
Martin Osama <martinosama2015@gmail.com>
2021-10-27 02:27:51 +0200

    getter and setter for Category

--- src/main/java/com/genesiscreations/kdec/model/Category.java
+++ src/main/java/com/genesiscreations/kdec/model/Category.java
@@ -9,4 +9,16 @@ public class Category {
             @GeneratedValue(strategy = GenerationType.IDENTITY)
     int id;
     String categoryName;
+
+    public Category(String categoryName) {
+        this.categoryName = categoryName;
+    }
+
+    public String getCategoryName() {
+        return categoryName;
+    }
+
+    public void setCategoryName(String categoryName) {
+        this.categoryName = categoryName;
+    }
 }
